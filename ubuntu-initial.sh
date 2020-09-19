e5572519e09764f2993f24a0ac6103c1faa990ae
Jeremy Howard <j@fast.ai>
2020-09-19 09:22:00 -0700

    Create ubuntu-initial.sh

---
+++ ubuntu-initial.sh
@@ -0,0 +1,24 @@
+export DEBIAN_FRONTEND=noninteractive
+sudo add-apt-repository -y ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get -y install apt-fast
+sudo apt-fast -y upgrade
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository -y https://cli.github.com/packages
+sudo apt-fast -y install vim-nox python3-powerline rsync ubuntu-drivers-common python3-pip ack-grep lsyncd wget bzip2 ca-certificates git rsync build-essential curl grep sed dpkg sudo libglib2.0-dev zlib1g-dev lsb-release tmux less htop ctags openssh-client python-is-python3 python3-pip python3-dev dos2unix gh pigz 
+
+cat << 'EOF' >> ~/.ssh/config
+Host *
+ServerAliveInterval 60
+StrictHostKeyChecking no
+Host github.com
+User git
+Port 22
+Hostname github.com
+TCPKeepAlive yes
+IdentitiesOnly yes
+EOF
+chmod 600 ~/.ssh/config
+
+# This will reboot your machine to install upgrades
+sudo shutdown -r now
