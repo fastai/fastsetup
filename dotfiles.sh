c303bcb263c792e495ae5d86d5e6afc096218f99
Jeremy Howard <j@fast.ai>
2020-09-19 21:25:14 +0000

    dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+set -e
+
+cd
+shopt -s expand_aliases
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> ~/.bashrc
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/fastai/dotfiles.git .cfg/
+config checkout
+config config --local status.showUntrackedFiles no
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "source ~/.bashrc.local" >> ~/.bashrc
+
+read -e -p "Enter your name (for git configuration): " NAME
+if [[ $NAME ]]; then
+  git config --global user.name $NAME
+fi
+read -e -p "Enter your email (for git configuration): " EMAIL
+if [[ $EMAIL ]]; then
+  git config --global user.email $EMAIL
+fi
+
--- ubuntu-initial.sh
+++ ubuntu-initial.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository -y ppa:apt-fast/stable
 sudo apt-get update
-sudo apt-get -y install apt-fast
-sudo apt-fast -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install apt-fast
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository -y https://cli.github.com/packages
 sudo apt-fast -y install vim-nox python3-powerline rsync ubuntu-drivers-common python3-pip ack-grep lsyncd wget bzip2 ca-certificates git rsync build-essential \
   curl grep sed dpkg sudo libglib2.0-dev zlib1g-dev lsb-release tmux less htop ctags openssh-client python-is-python3 python3-pip python3-dev dos2unix gh pigz 
+sudo apt-fast -y full-upgrade
 
 cat << 'EOF' >> ~/.ssh/config
 Host *
