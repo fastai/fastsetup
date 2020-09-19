5e1ca798be78d3bf19c298f1cb480caa90cb5d0d
Jeremy Howard <j@fast.ai>
2020-09-19 21:56:47 +0000

    source

--- setup-conda.sh
+++ setup-conda.sh
@@ -4,13 +4,13 @@ set -e
 cd
 
 case "$OSTYPE" in
-  darwin*)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; RC_FILE=.bash_profile ;;
-  linux*)   DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; RC_FILE=.bashrc  ;;
+  darwin*)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; ;;
+  linux*)   DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
 case "$SHELL" in
-  /bin/zsh*)   SHELL_NAME=zsh; RC_FILE=.zshrc ;;
+  /bin/zsh*)   SHELL_NAME=zsh; ;;
   /bin/bash*)  SHELL_NAME=bash ;;
   /usr/local/bin/fish*) SHELL_NAME=fish ;;
   *)        echo "unknown: $SHELL" ;;
@@ -29,9 +29,8 @@ bash Miniconda3-latest*.sh -b
 ~/miniconda3/bin/conda init $SHELL_NAME
 rm Miniconda3-latest*.sh
 
-perl -n  -e 'print if     />>> conda/../<<< conda/' $RCFILE > .condainit
-perl -ni -e 'print unless />>> conda/../<<< conda/' $RCFILE
-echo source ~/.condainit >> $RCFILE
-source ~/.condainit
+source ~/.bashrc
 
-conda install -y mamba
+conda install -yq mamba
+mamba install -yq fastcore
+
