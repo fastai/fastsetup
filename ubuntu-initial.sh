0a90d0897ead1c8304688bbe7fd5ca4a47b370f2
Jeremy Howard <j@fast.ai>
2020-09-19 19:21:25 -0700

    apt-fast

--- ubuntu-initial.sh
+++ ubuntu-initial.sh
@@ -9,17 +9,19 @@ sudo apt-add-repository -y https://cli.github.com/packages
 sudo apt-fast -y install vim-nox python3-powerline rsync ubuntu-drivers-common python3-pip ack-grep lsyncd wget bzip2 ca-certificates git rsync build-essential \
   curl grep sed dpkg sudo libglib2.0-dev zlib1g-dev lsb-release tmux less htop ctags openssh-client python-is-python3 python3-pip python3-dev dos2unix gh pigz 
 sudo apt-fast -y full-upgrade
+sudo cp apt-fast.conf /etc/
 
 cat << 'EOF' >> ~/.ssh/config
 Host *
-ServerAliveInterval 60
-StrictHostKeyChecking no
+  ServerAliveInterval 60
+  StrictHostKeyChecking no
+
 Host github.com
-User git
-Port 22
-Hostname github.com
-TCPKeepAlive yes
-IdentitiesOnly yes
+  User git
+  Port 22
+  Hostname github.com
+  TCPKeepAlive yes
+  IdentitiesOnly yes
 EOF
 chmod 600 ~/.ssh/config
 
