7fa81c45e05f69cfa2ca9e304597321e2b53f326
Jeremy Howard <j@fast.ai>
2020-09-27 07:54:15 -0700

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -16,11 +16,15 @@ fi
 echo "source ~/.bashrc.local" >> ~/.bashrc
 . ~/.bashrc
 
-read -e -p "Enter your name (for git configuration): " NAME
+if [[ -z $NAME ]]; then
+  read -e -p "Enter your name (for git configuration): " NAME
+fi
 if [[ $NAME ]]; then
   git config --global user.name "$NAME"
 fi
-read -e -p "Enter your email (for git configuration): " EMAIL
+if [[ -z $EMAIL ]]; then
+  read -e -p "Enter your email (for git configuration): " EMAIL
+fi
 if [[ $EMAIL ]]; then
   git config --global user.email "$EMAIL"
 fi
