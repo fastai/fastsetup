a2aaed42073ac6ebbccaaa1710f4755518324a79
Jeremy Howard <j@fast.ai>
2020-10-03 00:10:49 +0000

    open relay

--- opensmtpd-install.sh
+++ opensmtpd-install.sh
@@ -7,15 +7,29 @@ apt-fast install -y dkimproxy procmail
 apt remove -y amavisd-new
 apt -y autoremove
 
-cp smtpd.conf /etc/
-chmod 640 smtpd.conf
-chown root:root /etc/smtpd.conf
 DOMAIN=$(hostname -d)
+[[ -z $ROOTMAIL ]] && read -e -p "What email address would you like to send root email to: " ROOTMAIL
+echo "@: $ROOTMAIL" > /etc/aliases
+
+mkdir -p /etc/smtpd/
+[[ -z $SMTPPASS ]] && read -e -p "Enter password for accessing your SMTP server: " SMTPPASS
+PASS=$(smtpctl encrypt "$SMTPPASS")
+openssl req  -nodes -new -x509 -subj "/C=US/ST=CA/L=SF/O=$DOMAIN/OU=IT/CN=$DOMAIN" -keyout /etc/smtpd/smtpd.key -out /etc/smtpd/smtpd.crt
+chown root:opensmtpd /etc/smtpd/*
+chmod 640 /etc/smtpd/*
+cp smtpd.conf /etc/
+chmod 640 /etc/smtpd.conf
+chown root:root /etc/smtpd.conf
+perl -pi -e "s/SMTPPASS/$SMTPPASS/" /etc/smtpd.conf
+
 echo "domain  $DOMAIN" >> /etc/dkimproxy/dkimproxy_out.conf
 echo DKIMPROXYGROUP=ssl-cert >> /etc/default/dkimproxy
 
 systemctl restart opensmtpd.service
 systemctl restart dkimproxy.service
+
+echo "If you want to open your mail server to external IPs, run 'sudo ufw allow 587'"
+echo
 echo "This is your public key - copy it into a TXT DNS record 'postfix._domainkey.$DOMAIN':"
 perl -ne '!/^---/ && chomp && print' /var/lib/dkimproxy/public.key && echo
 
