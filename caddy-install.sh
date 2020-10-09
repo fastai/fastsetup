f84dc74706b60f640ccbba2d024e1f4245c63b24
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-10-09 13:11:18 +0200

    update caddy installation
    the Caddyfile is now generated by the script based on user input

--- caddy-install.sh
+++ caddy-install.sh
@@ -16,7 +16,59 @@ EOF
 sudo apt update
 sudo apt install -y caddy
 
-cp Caddyfile ~/
+read -e -p "We recommend setting a basic HTTP auth in Caddy. Set it now? [y/n] " -i y
+if [[ $REPLY = y* ]]; then
+  [[ -z $CADDYUSER ]] && read -e -p "Enter user for HTTP auth: " CADDYUSER
+  [[ -z $CADDYPASSWD ]] && read -e -p "Enter password for HTTP auth: " CADDYPASSWD
+  export CADDYHASHED=$(caddy hash-password --plaintext "$CADDYPASSWD")
+fi
+
+[[ -z $DOMAIN ]] && read -e -p "Enter domain name to set: " DOMAIN
+# TODO: check if domain has www. or not before
+[[ -z $UPSTREAMPORT ]] && read -e -p "Enter the port where your app is running: " UPSTREAMPORT
+
+cat << 'EOF' >> ~/Caddyfile
+$DOMAIN {
+  encode zstd gzip
+
+  # if you want to lead balance between different
+  # replicas of the app, you can add more <address>:<port>
+  # and caddy will load balance between them
+  reverse_proxy localhost:$UPSTREAMPORT {
+	lb_policy least_conn
+  }
+
+  # basic auth for the server, to change it:
+  # 1. run the command: caddy hash-password
+  # 2. enter the password
+  # 3. copy the hash and replace the one below
+  # 4. replace the user with the username you want
+  basicauth {
+      $CADDYUSER $CADDYHASHED
+  }
+
+
+  # log connections to the server
+  log {
+    output file /var/log/caddy.$DOMAIN.log {
+        roll_size 20MiB
+	roll_keep 2
+	roll_keep_for 720h
+    }
+  }
+}
+
+www.$DOMAIN {
+  redir https://$DOMAIN{uri} permanent
+}
+EOF
+
+read -e -p "Do you want to start Caddy now? [y/n] " -i y
+if [[ $REPLY = y* ]]; then
+  cd
+  caddy start
+fi
+
 
 # Caddy can be set up as a service: https://caddyserver.com/docs/install#install
 # but if it's installed from apt it does not seem to be needed
