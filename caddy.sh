91c9c791be830f00365ad2a2d65d9fb32b5d18d1
Jeremy Howard <j@fast.ai>
2020-11-21 07:21:24 -0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -11,6 +11,7 @@ esac
 
 rel=$(gh release list -R caddyserver/caddy | grep Latest | cut -f 1)
 wget -qO- "https://github.com/caddyserver/caddy/releases/latest/download/caddy_${rel:1}_${name}.tar.gz" | tar xz
+mv caddy /usr/bin/
 
 groupadd --system caddy
 useradd --system --gid caddy --create-home --home-dir /var/lib/caddy --shell /usr/sbin/nologin caddy
