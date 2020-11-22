919afd442098bdeaee557242c728f290c15e3eb6
Jeremy Howard <j@fast.ai>
2020-11-22 05:51:17 -0800

    Update getcaddy.sh

--- getcaddy.sh
+++ getcaddy.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 case "$OSTYPE" in
-  darwin*)  name=mac_amd64; ;;
-  linux*)   name=linux_amd64; ;;
-  msys*)    name=windows_amd64; ;;
+  darwin*)  name=mac_amd64;ext='.tar.gz'; ;;
+  linux*)   name=linux_amd64;ext='.tar.gz'; ;;
+  msys*)    name=windows_amd64;ext='.zip'; ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
-rel=$(wget -qO- https://api.github.com/repos/caddyserver/caddy/releases/latest | \
-  jq -r '.assets | .[] | select(.name | contains ("'${name}'") and contains (".tar.gz"))  | .browser_download_url')
-wget -qO- ${rel} | tar xz
+rels=https://api.github.com/repos/caddyserver/caddy/releases/latest
+rel=$(wget -qO- ${rels} | jq -r ".assets | .[] | select(.name | contains (\"${name}\") and contains (\"${ext}\"))  | .browser_download_url")
+
+if [[ $name == "windows_amd64" ]]; then
+  wget -qO- ${rel}
+  unzip *.zip
+else
+  wget -qO- ${rel} | tar xz
+fi
