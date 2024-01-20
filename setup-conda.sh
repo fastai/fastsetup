2354ea6b7de2fc876941f29378f2034ee1ca8544
Jeremy Howard <j@fast.ai>
2024-01-20 18:01:39 +1000

    miniconda

--- setup-conda.sh
+++ setup-conda.sh
@@ -4,13 +4,13 @@ set -eou pipefail
 case "$OSTYPE" in
   darwin*)
     case $(uname -m) in
-      arm64)  DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-arm64.sh; ;;
-      *)      DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-x86_64.sh; ;;
+      arm64)  DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh; ;;
+      *)      DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh; ;;
     esac ;;
   linux*)
     case $(uname -m) in
-      aarch64) DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-aarch64.sh; ;;
-      *)       DOWNLOAD=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh; ;;
+      aarch64) DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh; ;;
+      *)       DOWNLOAD=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh; ;;
       esac ;;
   *)          echo "unknown: $OSTYPE" ;;
 esac
@@ -24,9 +24,9 @@ esac
 
 echo Downloading installer...
 curl -LO --no-progress-meter $DOWNLOAD
-bash Mambaforge-*.sh -b
+bash Miniconda3-*.sh -b
 
-~/mambaforge/bin/conda init $SHELL_NAME
+~/miniconda3/bin/conda init $SHELL_NAME
 
 echo Please close and reopen your terminal.
 
