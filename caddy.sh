42b05a1b4c922cc23ada2fb85ac78c2d5b1e65ac
Jeremy Howard <j@fast.ai>
2024-10-15 13:15:26 +1000

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -3,6 +3,9 @@ set -e
 fail () { echo $1 >&2; exit 1; }
 [[ $(id -u) = 0 ]] || fail "Please run 'sudo $0'"
 
+# To custom compile dns providers, include e.g.:
+#  --with github.com/caddy-dns/azure --with github.com/caddy-dns/cloudflare --with github.com/caddy-dns/digitalocean --with github.com/caddy-dns/gandi --with github.com/caddy-dns/godaddy --with github.com/caddy-dns/googleclouddns --with github.com/caddy-dns/hetzner --with github.com/caddy-dns/linode --with github.com/caddy-dns/namecheap --with github.com/caddy-dns/netlify --with github.com/caddy-dns/ovh --with github.com/caddy-dns/powerdns --with github.com/caddy-dns/rfc2136 --with github.com/caddy-dns/route53 --with github.com/caddy-dns/vercel --with github.com/caddy-dns/vultr
+
 wget -qO- https://caddy.fast.ai | bash
 mv caddy /usr/bin/
 wget -q https://github.com/caddyserver/dist/blob/master/init/caddy.service
